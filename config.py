f317e594d4da1a41c702226abb7ce5ff5452a9a9
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-25 10:51:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ class Config:
     START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nIam A Simple Youtube to Mp3 Downloader Bot,</b>\n\nSend me Any Songs name with /song command")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2a35fca576aa49de77c98.jpg")
     OWNER = os.environ.get("OWNER", "shamilhabeeb") 
-    DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
+    DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "tvseriezzz_music")
     msg = {}
