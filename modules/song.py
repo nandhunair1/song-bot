ff5f31973f8210ab2fe2c68113d751d2149994ba
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-25 13:19:00 +0530

    Update song.py

--- modules/song.py
+++ modules/song.py
@@ -61,7 +61,7 @@ def a(client, message):
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            performer = f"[꧁࿗༒♛DQ♛༒࿗꧂]" 
+            performer = f"[𝕸𝖚𝖘𝖎𝖈]" 
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=False)
             open(thumb_name, 'wb').write(thumb.content)
