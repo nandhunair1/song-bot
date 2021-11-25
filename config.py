04fffe77171350e54c704f79e95c7762218a984c
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-25 10:19:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nIam A Simple Youtube to Mp3 Downloader Bot,</b>\n\nSend me Any Songs name, I can help you with uploading that to TG")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2a35fca576aa49de77c98.jpg")
-    OWNER = os.environ.get("OWNER", "shamilhabeeb") 
-    DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
+    OWNER = os.environ.get("OWNER", "MrC_VENOM") 
+    DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "tvseriezzz_music")
     msg = {}
