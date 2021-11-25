c38abe80f0efa36b3ca00616ff9e8e139f424f99
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-25 14:33:01 +0530

    Update welcome.py

--- modules/welcome.py
+++ modules/welcome.py
@@ -7,12 +7,38 @@ from pyrogram.types import Message, User
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot,message):
 	chatid= message.chat.id
-	await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
+	await bot.send_message(text=f"<b>Welcome {message.from_user.mention} to {message.chat.title} ,  Happy to have here</b>",
+        reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("📃 MUST READ | Click Here 📃", url="https://t.me/vayichitt_poyamathii")
+                        ],
+                        [
+                            InlineKeyboardButton("🔍 Click Here & Go To Google 🔎", url="https://www.google.com")
+                        ]
+                    ]
+                ),
+                parse_mode="markdown"
+            )
+        return
 	
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
 	chatid= message.chat.id
-	await bot.send_message(text=f"Bye ,  {message.from_user.mention} , Have a Nice Day",chat_id=chatid)
+	await bot.send_message(text=f"</b>Bye ,  {message.from_user.mention} , Have a Nice Day</b>",
+        reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("📃 MUST READ | Click Here 📃", url="https://t.me/vayichitt_poyamathii")
+                        ],
+                        [
+                            InlineKeyboardButton("🔍 Click Here & Go To Google 🔎", url="https://www.google.com")
+                        ]
+                    ]
+                ),
+                parse_mode="markdown"
+            )
+        return
 
 	
 
