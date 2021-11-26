1ac93d0d766f7738f33851005c5b924278b6ce22
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-26 22:16:52 +0530

    Update song.py

--- modules/song.py
+++ modules/song.py
@@ -38,7 +38,25 @@ def a(client, message):
     query =message.text
     print(query)
     m = message.reply('`🔎 𝗦𝗲𝗮𝗿𝗰𝗵𝗶𝗻𝗴 𝘁𝗵𝗲 𝗦𝗼𝗻𝗴.... 😌`')
-    ydl_opts = {"format": "bestaudio"}
+    ydl_opts = {
+            "format": "bestaudio",
+            "addmetadata": True,
+            "key": "FFmpegMetadata",
+            "writethumbnail": True,
+            "prefer_ffmpeg": True,
+            "geo_bypass": True,
+            "nocheckcertificate": True,
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "320",
+                }
+            ],
+            "outtmpl": "%(id)s.mp3",
+            "quiet": True,
+            "logtostderr": False,
+        }
     try:
         results = []
         count = 0
