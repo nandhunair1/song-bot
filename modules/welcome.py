384f6b57189f9461f1016c6b85a5bd1f4660ab13
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-30 10:35:15 +0530

    😁

--- modules/welcome.py
+++ modules/welcome.py
@@ -8,12 +8,14 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot,message):
 	chatid= message.chat.id
-	await bot.send_message(text=f"<b>Hello {message.from_user.mention} 🙂</b>\n\n<b>Welcome to {message.chat.title} ,  Happy to see you</b>",chat_id=chatid)
+	m = await bot.send_message(text=f"<b>Hello {message.from_user.mention} 🙂</b>\n\n<b>Welcome to {message.chat.title} ,  Happy to see you</b>",chat_id=chatid)
+        await asyncio.sleep(60)
+        await m.delete()
         
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
 	chatid= message.chat.id
-	await bot.send_message(text=f"<b>Bye ,  {message.from_user.mention} , Have a Nice Day</b>",chat_id=chatid)
+	m = await bot.send_message(text=f"<b>Bye ,  {message.from_user.mention} , Have a Nice Day</b>",chat_id=chatid)
 
 
 
