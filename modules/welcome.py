e07e85a44a72c1c1cf4751bbd4f9d52c61108951
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-30 11:21:32 +0530

    😣

--- modules/welcome.py
+++ modules/welcome.py
@@ -9,14 +9,14 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 async def welcome(bot,message):
 	chatid= message.chat.id
 	await bot.send_message(text=f"<b>Hello {message.from_user.mention} 🙂</b>\n\n<b>Welcome to {message.chat.title} ,  Happy to see you</b>",chat_id=chatid)
-        send_message.delete(60)
+        message.delete(60)
 
         
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
 	chatid= message.chat.id
 	await bot.send_message(text=f"<b>Bye ,  {message.from_user.mention} , Have a Nice Day</b>",chat_id=chatid)
-        send_message.delete(60)
+        
 
 
 
